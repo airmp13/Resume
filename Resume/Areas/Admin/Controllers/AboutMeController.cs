a853cebf3d622e82d5ef238f771de0ede3c3df09
aimz <airmp13@yahoo.com>
2023-11-21 01:44:11 +0330

    Add AboutMe Controller in adminpanel

---
+++ Resume/Areas/Admin/Controllers/AboutMeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Resume.Application.Services.Implements;
+using Resume.Application.Services.Interfaces;
+
+namespace Resume.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AboutMeController : Controller
+    {
+        private readonly IAboutMeService _aboutMeService;
+
+        public AboutMeController(IAboutMeService aboutMeService)
+        {
+            _aboutMeService = aboutMeService;
+            
+        }
+        public async Task<IActionResult> Index()
+        {
+            
+            return View(await _aboutMeService.GetAboutMesDTOAsync());
+        }
+    }
+}
